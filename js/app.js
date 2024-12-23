4199a78df98cfbf4107f0f37611bcb20ec82b3c7
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-23 23:58:42 +0200

    pop_up_window

--- js/app.js
+++ js/app.js
@@ -13,7 +13,13 @@ window.onload = function () {
 
 }
 
-
+import PhotoSwipeLightbox from '../libs/PhotoSwipe/photoswipe-lightbox.esm.min.js'
+const lightbox = new PhotoSwipeLightbox({
+	gallery: '.gallery',
+	children: '.gallery__item',
+	pswpModule: () => import('../libs/PhotoSwipe/photoswipe.esm.min.js')
+})
+lightbox.init()
 
 /* !new! */
 
